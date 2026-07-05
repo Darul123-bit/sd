fdac4efb16f4d9a54c5ab4581201ad9da96140e9
Darul123-bit <emmanuelbayaro09@gmail.com>
2026-07-05 21:59:22 +0000

    Initial commit

---
+++ ex/chap1/exo1.py
@@ -0,0 +1,18 @@
+# Python comme une calculatrice
+
+# Prévoyons sans exécuter l'évaluation des expressions arithmétiques suivantes:
+ 
+'''
+1. 6 * 7 + 3 = 45
+
+2. 6 * (7 + 3) = 60
+
+3. 45 // 7 = 6
+
+4. 3 * 7 // 4 = 5
+
+5. (3 * 7) // 4 = 5
+
+6. (45 // 7) * 7 + 45 % 7 = 45
+
+'''
---
+++ ex/chap1/exo3.py
@@ -0,0 +1,15 @@
+# Chaines de caractères
+
+"""
+1. l'opérateur '+' entre deux chaines de caractères fait la concaténation
+
+2. l'opérateur '*' entre une chaine de caractères et un nombre est la répétition
+
+3. "a" * 2 + "b" * 3 + "c" * 4 est bien valide
+
+4. les opérateurs '-' et '/' ne peuvent pas être utiliser avec les chaines de caractères
+car elles provoquent un TypeError: unsupported operand type(s) for -: 'str' and 'int'
+
+5. 'ab' * 5 == 'a' + 'ba' * 4 + 'b' vaut True
+
+"""
