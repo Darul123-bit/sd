a7f09165cc685595ab1d2303d7755198ed9b3465
Darul123-bit <emmanuelbayaro09@gmail.com>
2026-07-11 15:15:52 +0000

    add file ex/chap2/exo2.py and revision/revision_1.py

--- revision/revision_1.py
+++ revision/revision_1.py
@@ -2,8 +2,25 @@
 
 #   Listes comme structures de données et dictionnaires comme structure de données
 
-chaine = input("Veuillez entre la chaine à analyser : ")
+estValide = True
 
-chaine = chaine.lower()
+try:
+    while estValide:
+        chaine = input("Veuillez entrer la chaine à analyser : ")
+        estValide = chaine.isalpha
+        chaine = chaine.lower()
+        chaine = "".join(chaine.split())
+        
+        if estValide:
+            print(chaine.count('a'))
+            print(chaine)
+            break
+        
+        estValide = True
+except:
+    print("Veuillez taper uniquement une chaine de caractère sans caractères spéciaux ni espace")  
 
-print(chaine)
+
+
+
+
